c0b5cf83fdbbcbc841c0e385eb1ebf600b4f3176
AnJW-HGU <22000404@handong.edu>
2021-12-06 02:12:23 +0900

    feat: Complete making python web

--- project/app.py
+++ project/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
+model = pickle.load(open('musicModel.pkl', 'rb'))
+
+def find_similar_song(name, n=5):
+    series = model[name].sort_values(ascending=False)
+    series = series.drop(name)
+    return series.head(n).to_frame()
 
 @app.route('/')
 def home():
@@ -14,7 +22,10 @@ def my():
 def result():
     if request.method == 'POST':
         result = request.form
-        return render_template("result.html", result = result)
+        songName = result['genre'] +'.000'+ result['selectNumber'] +'.wav'
+        songList = find_similar_song(songName)
+        return songList.to_html(classes='table table-striped text-center', justify='center')
+        # return render_template("result.html", result = songList)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', post=5000, debug=True)
